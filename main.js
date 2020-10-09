2a1c6f20937631ea178e518378b03559c145aa3a
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-09 18:34:35 +0000

    code clean up

--- main.js
+++ main.js
@@ -20,26 +20,6 @@ function addItem (id, name, description, price, moreInfo){
   $('#container').prepend(html);
 }
 $(document).ready(function (){
-  /*$('#button-create-item').on('click', function (){
-    let name = $('#input-create-item').val();
-    $('#input-create-item').val('');
-      });*/
-
-    /*let html  = '';
-    html += '<div class="item">';
-    html += '<div class="name">' + name + '</div>';
-    html += '<img src="nyc.jpg"></img>';
-    html += '<div class="description">One of those busy cities</div>';
-    html += '<div class="price">500</div>';
-    html += '<button class="item-add">Add to cart</button>';
-    html += '<button class="item-remove">Remove</button>';
-    html += '<br>';
-    html += '<a  class="more-info-link" href="#">More info</a>';
-    html += '<div class="more-info">Still one of those busy cities</div>';
-    html += '</div>';*/
-
-
-
 
   $('#container').on('click','.more-info-link',function(event){
     event.preventDefault();
@@ -110,7 +90,7 @@ $(document).ready(function (){
         })
         .done(function(response){
           $('#feedback-message').text(response.message);
-        //  alert(data.form + ':' + response.message);
+      
         });
       });
 
