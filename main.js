e5a13ba567c9c21359a281df7047cb138649d345
Bensille Bruce Aburiyna <bensille.aburinya@amalitech.org>
2020-10-09 15:46:15 +0000

    loading jsons

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
+function addItem (name, description, price, moreInfo){
+  let html  = '';
+  html += '<div class="item">';
+  html += '<div class="name">' + name + '</div>';
+  html += '<img src="nyc.jpg"></img>';
+  html += '<div class="description">' + description + '</div>';
+  html += '<div class="price">' + price + '</div>';
+  html += '<button class="item-add">Add to cart</button>';
+  html += '<button class="item-remove">Remove</button>';
+  html += '<br>';
+  html += '<a  class="more-info-link" href="#">More info</a>';
+  html += '<div class="more-info">' + moreInfo + '</div>';
+  html += '</div>';
+
+  $('#container').prepend(html);
+}
 $(document).ready(function (){
-  $('#button-create-item').on('click', function (){
+  /*$('#button-create-item').on('click', function (){
     let name = $('#input-create-item').val();
     $('#input-create-item').val('');
+      });*/
 
-    let html  = '';
+    /*let html  = '';
     html += '<div class="item">';
     html += '<div class="name">' + name + '</div>';
     html += '<img src="nyc.jpg"></img>';
@@ -14,10 +31,10 @@ $(document).ready(function (){
     html += '<br>';
     html += '<a  class="more-info-link" href="#">More info</a>';
     html += '<div class="more-info">Still one of those busy cities</div>';
-    html += '</div>';
+    html += '</div>';*/
 
-    $('#container').prepend(html);
-  });
+
+
 
   $('#container').on('click','.more-info-link',function(event){
     event.preventDefault();
@@ -31,6 +48,19 @@ $(document).ready(function (){
       $(this).parent().remove();
     });
 
-    $.ajax('item.json', function(response){});
+    $.ajax('item.json', {dataType: 'json', contentType:'application/json',
+      cache: false})
+      .done(function(response){
+        let items = response.items;
+        items.forEach(function(item){
+          addItem(item.name, item.description, item.price, item.moreInfo);
+        });
+      })
+      .fail(function(request, errorType, errorMessage){
+        console.log(errorMessage);
+      })
+      .always(function(){
+
+      })
 
 });
